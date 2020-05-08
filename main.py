5bc4eed9e809a53fe72ef8933c3413c272e93a0d
Philipoff <Filippok2002@yandex.ru>
2020-05-08 03:49:23 +0300

    Messages between VK and TG

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from panel import app
 from bot.bot import main as mainVK
 from bot.tg_bot import main as mainTG
-from bot.checker import additionalVK
+from bot.checker import main as mainChecker
 from multiprocessing import Process
 
 __VERSION__ = '0.2'
@@ -9,10 +9,10 @@ __VERSION__ = '0.2'
 if __name__ == '__main__':
     # panel_process = Process(target=app.main)
     VK_process = Process(target=mainVK)
-    # VK_loader_process = Process(target=additionalVK)
+    VK_loader_process = Process(target=mainChecker)
     TG_process = Process(target=mainTG)
 
     VK_process.start()
     # panel_process.start()
     TG_process.start()
-    # VK_loader_process.start()
+    VK_loader_process.start()
